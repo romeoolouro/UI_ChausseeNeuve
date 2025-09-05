d808470a2a7a90eca918f57289c2853910d6d38f
elonmj <josaphatahouanye@gmail.com>
2025-09-05 09:48:36 +0100

    fix: corriger les boutons de température et fréquence dans la vue bibliothèque
    - Corriger l'accès au DataContext dans MaterialSelectionControl pour les boutons température/fréquence
    - Remplacer ToList().IndexOf() par Array.IndexOf() pour éviter RuntimeBinderException
    - Utiliser les méthodes de tableau natives au lieu des méthodes d'extension LINQ avec dynamic
    - Ajouter l'import System pour Array.IndexOf()
    - Mettre à jour les handlers OnTemperatureDown/Up et OnFrequenceDown/Up
    - Corriger la notification du parent ViewModel lors de la sélection de matériau
    - Résoudre les erreurs de compilation et d'exécution liées aux tableaux dynamiques

--- UI_ChausseeNeuve/Views/MaterialSelectionControl.xaml.cs
+++ UI_ChausseeNeuve/Views/MaterialSelectionControl.xaml.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using UI_ChausseeNeuve.ViewModels;
 
 namespace UI_ChausseeNeuve.Views
@@ -28,54 +30,67 @@ namespace UI_ChausseeNeuve.Views
         // Gestionnaire d'événement pour la sélection de matériau
         private void MaterialSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (sender is ListBox listBox && listBox.SelectedItem != null)
+            if (sender is ListBox listBox && listBox.SelectedItem != null && listBox.SelectedItem is MaterialItem materialItem)
             {
                 // Notification via ToastService si nécessaire
-                var viewModel = DataContext as BibliothequeViewModel;
-                viewModel?.OnMaterialSelected(listBox.SelectedItem);
+                // Since DataContext is the material view model, we need to access the parent view model
+                // The CurrentMaterialViewModel property contains the same instance, so we can use that
+                var parent = VisualTreeHelper.GetParent(this) as FrameworkElement;
+                while (parent != null && !(parent.DataContext is BibliothequeViewModel))
+                {
+                    parent = VisualTreeHelper.GetParent(parent) as FrameworkElement;
+                }
+                if (parent?.DataContext is BibliothequeViewModel bibVM)
+                {
+                    bibVM.OnMaterialSelected(materialItem);
+                }
             }
         }
 
         // Ajout des handlers pour les flèches température
         private void OnTemperatureDown(object sender, RoutedEventArgs e)
         {
-            var vm = (this.DataContext as BibliothequeViewModel)?.CurrentMaterialViewModel as dynamic;
+            var vm = this.DataContext as dynamic;
             if (vm?.TemperatureOptions != null && vm?.SelectedTemperature != null)
             {
-                int idx = vm.TemperatureOptions.ToList().IndexOf(vm.SelectedTemperature);
+                var options = (int[])vm.TemperatureOptions;
+                int idx = Array.IndexOf(options, vm.SelectedTemperature);
                 if (idx > 0)
-                    vm.SelectedTemperature = vm.TemperatureOptions[idx - 1];
+                    vm.SelectedTemperature = options[idx - 1];
             }
         }
         private void OnTemperatureUp(object sender, RoutedEventArgs e)
         {
-            var vm = (this.DataContext as BibliothequeViewModel)?.CurrentMaterialViewModel as dynamic;
+            var vm = this.DataContext as dynamic;
             if (vm?.TemperatureOptions != null && vm?.SelectedTemperature != null)
             {
-                int idx = vm.TemperatureOptions.ToList().IndexOf(vm.SelectedTemperature);
-                if (idx < vm.TemperatureOptions.Length - 1)
-                    vm.SelectedTemperature = vm.TemperatureOptions[idx + 1];
+                var options = (int[])vm.TemperatureOptions;
+                int idx = Array.IndexOf(options, vm.SelectedTemperature);
+                if (idx < options.Length - 1)
+                    vm.SelectedTemperature = options[idx + 1];
             }
         }
         // Ajout des handlers pour les flèches fréquence
         private void OnFrequenceDown(object sender, RoutedEventArgs e)
         {
-            var vm = (this.DataContext as BibliothequeViewModel)?.CurrentMaterialViewModel as dynamic;
+            var vm = this.DataContext as dynamic;
             if (vm?.FrequenceOptions != null && vm?.SelectedFrequence != null)
             {
-                int idx = vm.FrequenceOptions.ToList().IndexOf(vm.SelectedFrequence);
+                var options = (int[])vm.FrequenceOptions;
+                int idx = Array.IndexOf(options, vm.SelectedFrequence);
                 if (idx > 0)
-                    vm.SelectedFrequence = vm.FrequenceOptions[idx - 1];
+                    vm.SelectedFrequence = options[idx - 1];
             }
         }
         private void OnFrequenceUp(object sender, RoutedEventArgs e)
         {
-            var vm = (this.DataContext as BibliothequeViewModel)?.CurrentMaterialViewModel as dynamic;
+            var vm = this.DataContext as dynamic;
             if (vm?.FrequenceOptions != null && vm?.SelectedFrequence != null)
             {
-                int idx = vm.FrequenceOptions.ToList().IndexOf(vm.SelectedFrequence);
-                if (idx < vm.FrequenceOptions.Length - 1)
-                    vm.SelectedFrequence = vm.FrequenceOptions[idx + 1];
+                var options = (int[])vm.FrequenceOptions;
+                int idx = Array.IndexOf(options, vm.SelectedFrequence);
+                if (idx < options.Length - 1)
+                    vm.SelectedFrequence = options[idx + 1];
             }
         }
     }
